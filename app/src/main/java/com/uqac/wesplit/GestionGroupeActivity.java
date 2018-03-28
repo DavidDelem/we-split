93911002fae2c8093d4885f638730f2cc00b2a4e
DavidDel <david.delemotte@gmail.com>
2018-03-28 17:33:41 -0400

    new activities

---
+++ app/src/main/java/com/uqac/wesplit/GestionGroupeActivity.java
@@ -0,0 +1,13 @@
+package com.uqac.wesplit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GestionGroupeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_groupe);
+    }
+}
---
+++ app/src/main/java/com/uqac/wesplit/ParamsActivity.java
@@ -0,0 +1,13 @@
+package com.uqac.wesplit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParamsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_params);
+    }
+}
