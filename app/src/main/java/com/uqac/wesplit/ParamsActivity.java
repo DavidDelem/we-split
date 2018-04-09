c69ee3bb34e19c9263780ddfb7d07845350791d1
Paul <paul.du56@gmail.com>
2018-04-09 14:19:19 -0400

    Début page gestion param

--- app/src/main/java/com/uqac/wesplit/ParamsActivity.java
+++ app/src/main/java/com/uqac/wesplit/ParamsActivity.java
@@ -2,14 +2,25 @@ package com.uqac.wesplit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ParamsActivity extends AppCompatActivity {
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_params);
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputEmail.setText(auth.getCurrentUser().getEmail());
         // A FAIRE:
 
         // modifier mon adresse mail (a faire avec les fonctions de firebase)
